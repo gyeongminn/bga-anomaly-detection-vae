13959501a3d30478a812cb3652c899ac1083cd68
Gyeongmin <lee_gyeongmin@naver.com>
2023-10-10 02:44:51 +0900

    Create model report

--- test/5.cosine_similarilty.py
+++ test/5.cosine_similarilty.py
@@ -7,15 +7,7 @@ from src.visualization.image import show_images_with_data
 from src.visualization.result import show_roc_curve
 from src.visualization.plot import show_score_density_plot
 
-
-def cosine_score(data1, data2):
-    np_config.enable_numpy_behavior()
-    result = []
-    for x, y in zip(data1, data2):
-        similarity = cosine_similarity(x.reshape(1, -1), y.reshape(1, -1))[0][0]
-        result.append(similarity)
-    return result
-
+np_config.enable_numpy_behavior()
 
 if __name__ == "__main__":
     vae = VaeAgent()
@@ -24,7 +16,11 @@ if __name__ == "__main__":
     x_test, y_test = data_loader.load_test_data()
 
     gen_data = vae.predict(x_test)
-    scores = cosine_score(x_test, gen_data)
+
+    scores = []
+    for x, y in zip(x_test, gen_data):
+        similarity = cosine_similarity(x.reshape(1, -1), y.reshape(1, -1))[0][0]
+        scores.append(similarity)
 
     show_roc_curve(y_test, scores)
 
@@ -36,7 +32,9 @@ if __name__ == "__main__":
 
     show_score_density_plot(class_scores)
 
-    for i in range(len(vae.class_names)):
-        show_images_with_data(
-            vae.class_names[i], class_images[i], class_scores[i], is_float=True
-        )
+    show_image = False
+    if show_image:
+        for i in range(len(vae.class_names)):
+            show_images_with_data(
+                vae.class_names[i], class_images[i], class_scores[i], is_float=True
+            )
